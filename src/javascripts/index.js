939b73c37d3b3056a2814c2a4b19ddc21af9af05
Terra Tretter <terratretter@mail.weber.edu>
2020-03-20 18:16:18 -0600

    final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,18 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+function displayMessage() {
+  let name = document.getElementById("name-f").value;
+  let myText = "Hello " + name + ", Welcome!"
+  alert (myText);
+}
+
+function showMessage(){
+  alert("Display a message please im begging ");
+}
+function please(){
+  alert('Hello ' + document.getElementById('name-f').value );
+}
+
+//nothign i do works and i give up
